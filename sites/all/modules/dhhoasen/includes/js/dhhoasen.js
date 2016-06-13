fff70001cca8908a7d6e4a5c1b5000626a0516fa
Long Tran <tranlonguit29@gmail.com>
2016-06-13 23:19:38 +0700

    add project

---
+++ sites/all/modules/dhhoasen/includes/js/dhhoasen.js
@@ -0,0 +1,10 @@
+(function(jQuery, Drupal) {
+  Drupal.behaviors.telesales_behavior = {
+    attach: function (context, settings) {
+    /*
+     * code js here
+     */
+    }
+  }
+}(jQuery, Drupal));
+
